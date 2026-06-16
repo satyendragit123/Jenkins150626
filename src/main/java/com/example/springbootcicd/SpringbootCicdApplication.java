0d29a4e8fb6d03d0d44aee68818654cc8237ffd9
root <root@ip-10-0-1-7.ap-south-1.compute.internal>
2026-06-16 04:21:05 +0000

    Initial commit

---
+++ src/main/java/com/example/springbootcicd/SpringbootCicdApplication.java
@@ -0,0 +1,12 @@
+package com.example.springbootcicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCicdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootCicdApplication.class, args);
+    }
+}
